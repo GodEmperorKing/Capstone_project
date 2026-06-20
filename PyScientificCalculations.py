0b93d1a3b9ebd25714162300e235c42f1920cbfb
Eirik Collins <eirik.collins@gmail.com>
2026-06-19 18:35:21 -0600

    first spot of code

---
+++ PyScientificCalculations.py
@@ -0,0 +1,13 @@
+import math
+
+# This function returms the tan of x
+def tan(x):
+    return math.tan(x)
+
+# This function returms the sin of x
+def sin(x):
+    return math.sin(x)
+
+# This function returms the cos of x
+def cos(x):
+    return math.cos(x)
